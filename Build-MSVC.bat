9c82a8a3cf55c56489c91dbedc920f5bfce16352
Joey Leavell <joeyleavel@gmail.com>
2021-02-25 20:22:10 -0600

    Added built scripts

---
+++ Build-MSVC.bat
@@ -0,0 +1,13 @@
+rmdir /S /Q Build/MSVC
+rmdir /S /Q Install/MSVC
+cmake -D CMAKE_INSTALL_PREFIX="./Install/MSVC" -H. -BBuild/MSVC
+cmake --build ./Build/MSVC --config Release
+cmake --install ./Build/MSVC --config Release
+
+:: Copy the artifacts over
+mkdir ..\\Extern\\ShaderConductor\\Libraries\\x64\\Windows\\MSVC\\
+mkdir ..\\Extern\\ShaderConductor\\Binaries\\x64\\Windows\\MSVC\\
+mkdir ..\\Extern\\ShaderConductor\\Include
+copy /y .\Build\MSVC\Bin\Release\ShaderConductor.dll ..\Extern\ShaderConductor\Binary\x64\Windows\MSVC\ShaderConductor.dll
+copy /y .\Build\MSVC\Lib\Release\ShaderConductor.lib ..\Extern\ShaderConductor\Libraries\x64\Windows\MSVC\ShaderConductor.lib
+copy /y .\Include\ShaderConductor\ShaderConductor.hpp ..\Extern\ShaderConductor\Include\ShaderConductor.hpp
